3a019ebc3f014ff90a19a0d8efa840859b8b2a86
udith444 <foggmaster99@gmail.com>
2025-12-03 14:58:07 +0530

    assignment-5-complete latest

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'd32e6bab2904b4fec62f9a3048544f8d380e7903'
+AESD_ASSIGNMENTS_VERSION = '2516a21dd975fe2c7a14ec68769fc51e45e7d233'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,7 +15,7 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(TARGET_CC) $(TARGET_CFLAGS) $(@D)/server/aesdsocket.c -o $(@D)/server/aesdsocket
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -28,5 +28,6 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
+
 
 
